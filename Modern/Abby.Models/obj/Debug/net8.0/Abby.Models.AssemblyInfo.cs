8d313694b1b4a1438fd94e51a2a727e2862a0df1
Mikemike8 <Michaeljh811@gmail.com>
2024-07-27 09:30:16 -0500

    ok

--- Modern/Abby.Models/obj/Debug/net8.0/Abby.Models.AssemblyInfo.cs
+++ Modern/Abby.Models/obj/Debug/net8.0/Abby.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abby.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e2fb73ed0560addc340adc0589fce94b3ff85b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a203b89b9e99ae9ce52d9ba158673bf073c7be78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Abby.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Abby.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
